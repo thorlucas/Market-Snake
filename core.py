93fdf9dbb0ee7c42debbcca1f498683d90ea377f
Thor Lucas Correia <thor.lucas@icloud.com>
2018-10-09 11:10:58 -0700

    __getitem__ by timestamp, and comparisons directly to datetimes

--- core.py
+++ core.py
@@ -17,15 +17,30 @@ class AbstractPeriod(object):
 
 	## Returns whether or not this is newer than the other.
 	def __lt__(self, other):
-		return self.timestamp > other.timestamp
+		if isinstance(other, self.__class__):
+			return self.timestamp > other.timestamp
+		elif isinstance(other, datetime.datetime):
+			return self.timestamp > other
+		else:
+			raise TypeError('Cannot compare type %s with %s' % (other.__class__, self.__class__))
 
 	## Returns whether or not this is older than the other.
 	def __gt__(self, other):
-		return self.timestamp < other.timestamp
+		if isinstance(other, self.__class__):
+			return self.timestamp < other.timestamp
+		elif isinstance(other, datetime.datetime):
+			return self.timestamp < other
+		else:
+			raise TypeError('Cannot compare incompatible types %s with %s' % (other.__class__, self.__class__))
 
 	## Returns whether or not this is equal to the other.
 	def __eq__(self, other):
-		return self.timestamp == other.timestamp
+		if isinstance(other, self.__class__):
+			return self.timestamp == other.timestamp
+		elif isinstance(other, datetime.datetime):
+			return self.timestamp == other
+		else:
+			raise TypeError('Cannot compare incompatible types %s with %s' % (other.__class__, self.__class__))
 
 	def __hash__(self):
 		return self.timestamp.__hash__()
@@ -63,7 +78,12 @@ class AbstractTimeSeries(object):
 	## Gets a time period.
 	#  Index 0 is the newest period.
 	def __getitem__(self, index):
-		return self.periods[index]
+		if isinstance(index, (int, slice)):
+			return self.periods[index]
+		elif isinstance(index, datetime.datetime):
+			return self.periods[self.periods.index(index)]
+		else:
+			raise KeyError("Cannot fetch item from %s with key type %s" % (self.__class__, type(index)))
 
 	## Returns whether or not the series contains a period.
 	#  Note: This does not check the contents or type of period,
@@ -112,11 +132,23 @@ class AbstractTimeSeries(object):
 class CompoundTimeSeries(object):
 	## Creates a compound time series.
 	#  @param series is a dictionary of named series to be accessed by __getattr__.
-	def __init__(self, series):
+	def __init__(self, series, periodType = AbstractPeriod):
 		self.series = series
+		self.periodType = periodType
 
 	def __getattr__(self, key):
 		return self.series[key]
+
+	def __getitem__(self, index):
+		# TODO: Implement int version!
+		# The int version should account for offset time series, etc.
+		# It's going to be a little complex.
+		if isinstance(index, datetime.datetime):
+			return self.periodType(index, { k : v[index] for k, v in self.series.items()})
+		elif isinstance(index, (int, slice)):
+			raise NotImplementedError("Fetching by type %s not implemented yet." % type(index))
+		else:
+			raise TypeError("Cannot fetch item from %s with key type %s" % (self.__class__, type(index)))
 
 	def __str__(self):
 		# TODO: Broken for some fuckin' reason
